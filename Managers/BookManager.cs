acb0cd40c6bfde646d7cda1eeb1aea31326e870f
Daniel N. De Lara <dndelara@proton.me>
2024-10-20 02:58:50 +0800

    Added book manager and service (was ignored previously)

---
+++ Managers/BookManager.cs
@@ -0,0 +1,39 @@
+﻿using Managers.Services;
+using Models;
+
+namespace Managers
+{
+    public class BookManager : IBookService
+    {
+        private readonly List<Book> _books;
+
+        public BookManager()
+        {
+            _books = [];
+        }
+
+        public void AddBook(Book book)
+        {
+            book.Id = _books.Count + 1;
+            _books.Add(book);
+        }
+
+        public Book? GetBook(int id)
+        {
+            return (id >= 0 && id < _books.Count) ? _books[id] : null;
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return _books;
+        }
+
+        public Book? UpdateSummary(int id, string summary)
+        {
+            if (id < 0 || id >= _books.Count) return null;
+
+            _books[id].Summary = summary;
+            return _books[id];
+        }
+    }
+}
---
+++ Managers/Services/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using Models;
+
+namespace Managers.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetBooks();
+        Book? GetBook(int id);
+        void AddBook(Book book);
+        Book? UpdateSummary(int id, string summary);
+    }
+}
