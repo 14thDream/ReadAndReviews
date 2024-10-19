6861eb24471ff48b990f11529de21cb2cd2b69b6
Daniel N. De Lara <dndelara@proton.me>
2024-10-20 03:40:48 +0800

    Added Account Manager and Service

---
+++ Managers/AccountManager.cs
@@ -0,0 +1,33 @@
+﻿using Managers.Services;
+using Models;
+
+namespace Managers
+{
+    public class AccountManager : IAccountService
+    {
+        private readonly Dictionary<string, Account> _accounts;
+
+        public AccountManager()
+        {
+            _accounts = [];
+        }
+
+        public Account? AddAccount(Account account)
+        {
+            if (_accounts.ContainsKey(account.Email)) return null;
+
+            _accounts.Add(account.Email, account);
+            return account;
+        }
+
+        public bool DeleteAccount(string email)
+        {
+            return _accounts.Remove(email);
+        }
+
+        public Account? GetAccount(string email)
+        {
+            return _accounts.GetValueOrDefault(email);
+        }
+    }
+}
---
+++ Managers/Services/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using Models;
+
+namespace Managers.Services
+{
+    public interface IAccountService
+    {
+        Account? AddAccount(Account account);
+        bool DeleteAccount(string email);
+        Account? GetAccount(string email);
+
+    }
+}
