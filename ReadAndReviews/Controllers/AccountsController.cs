e9da3287200d7684bc8a4e77a96c85888d72f8f4
Daniel N. De Lara <dndelara@proton.me>
2024-10-20 03:40:55 +0800

    Added Account Controller

---
+++ ReadAndReviews/Controllers/AccountsController.cs
@@ -0,0 +1,44 @@
+﻿using Managers.Services;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace ReadAndReviews.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountsController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet("{email}")]
+        public ActionResult<Account> GetAccount(string email)
+        {
+            return _accountService.GetAccount(email) switch
+            {
+                null => NotFound(),
+                var account => Ok(account)
+            };
+        }
+
+        [HttpPut("{email}")]
+        public ActionResult AddAccount(Account account)
+        {
+            return _accountService.AddAccount(account) switch
+            {
+                null => NotFound(),
+                _ => CreatedAtAction(nameof(GetAccount), new { email = account.Email }, account)
+            };
+        }
+
+        [HttpDelete("{email}")]
+        public ActionResult DeleteAccount(string email)
+        {
+            return (_accountService.DeleteAccount(email)) ? NoContent() : NotFound();
+        }
+    }
+}
