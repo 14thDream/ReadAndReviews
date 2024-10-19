7230f6b1816a6b93cdf3a61fc4957f313b8d1107
Daniel N. De Lara <dndelara@proton.me>
2024-10-20 02:31:29 +0800

    Added setter to Book's Id

--- Models/Book.cs
+++ Models/Book.cs
@@ -15,7 +15,7 @@
             Summary = summary;
         }
 
-        public int Id { get; }
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string? Summary { get; set; }
